7702d37afa09194178366dee5bae7c816f4048fc
Ahmedkhan90 <dx.ahmedkhan@gmail.com>
2021-02-24 17:35:34 +0500

    Updates

---
+++ precache-manifest.f0fab9385cad671eecc883b880606f6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687be9f41415e320f3d7ae97a0c1c2e8",
+    "url": "/React-Authentication-Login-Password/index.html"
+  },
+  {
+    "revision": "2888f80ea0ad1a034718",
+    "url": "/React-Authentication-Login-Password/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4c91f583a7df8badaa61",
+    "url": "/React-Authentication-Login-Password/static/css/main.c60cfa8d.chunk.css"
+  },
+  {
+    "revision": "2888f80ea0ad1a034718",
+    "url": "/React-Authentication-Login-Password/static/js/2.d79012f6.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/React-Authentication-Login-Password/static/js/2.d79012f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c91f583a7df8badaa61",
+    "url": "/React-Authentication-Login-Password/static/js/main.1591939a.chunk.js"
+  },
+  {
+    "revision": "dd58e72668d40e39b3a9",
+    "url": "/React-Authentication-Login-Password/static/js/runtime-main.242a5b81.js"
+  }
+]);
